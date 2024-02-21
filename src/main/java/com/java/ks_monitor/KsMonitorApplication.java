1a8dcf95892765a49a6c972f9b3ac5b31e264711
OppSpark <picks5@naver.com>
2024-02-22 02:38:06 +0900

    first commit

---
+++ src/main/java/com/java/ks_monitor/KsMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.java.ks_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KsMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/ks_monitor/KsMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.ks_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
